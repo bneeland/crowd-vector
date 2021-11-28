5411f0c5571c858959967b89b53a9fc64bd89eea
Brian Neeland <brian.neeland@protonmail.com>
2021-11-27 23:30:28 -0700

    Added help text

--- simple_gifts/forms.py
+++ simple_gifts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class AssignerForm(forms.Form):
-    names = forms.CharField(widget=forms.Textarea)
-    emails = forms.CharField(widget=forms.Textarea)
-    inclusions = forms.CharField(widget=forms.Textarea, required=False)
-    exclusions = forms.CharField(widget=forms.Textarea, required=False)
+    names = forms.CharField(widget=forms.Textarea, help_text='Enter each name on a separate line.')
+    emails = forms.CharField(widget=forms.Textarea, help_text='Enter each email, in the same order as the names, on a separate line.')
+    inclusions = forms.CharField(widget=forms.Textarea, help_text='Enter the name of the person who must send, then a comma, then the name of the person who must receive from that sender. The format should look like: SENDER_NAME,RECEIVER_NAME. One sender-receiver pair per line.', required=False)
+    exclusions = forms.CharField(widget=forms.Textarea, help_text='Enter the name of the person who shall not send, then a comma, then the name of the person who must not receive from that sender. The format should look like: SENDER_NAME,RECEIVER_NAME. One sender-receiver pair per line.', required=False)
